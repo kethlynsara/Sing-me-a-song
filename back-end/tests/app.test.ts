fbd2ac58c826b0a26788a4c48e79691e1d38f306
Kethlyn <kethlynsaraa@gmail.com>
2022-07-26 20:36:05 -0300

    feat: tests setup

---
+++ back-end/tests/app.test.ts
@@ -0,0 +1,12 @@
+import supertest from "supertest";
+import app from "../src/app.js";
+import { prisma } from "../src/database.js"
+
+const RIGHT_LINK = "https://www.youtube.com/watch?v=rdgMPa8C-dc"
+const WRONG_LINK = "https://www.youtube"
+
+describe("recommendations tests", () => {
+    it("", async () => {
+        
+    })
+})
