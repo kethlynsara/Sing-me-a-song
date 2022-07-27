89715611e73f2723b5acf14b4e0ac9b3ebcfd5f3
Kethlyn <kethlynsaraa@gmail.com>
2022-07-27 16:16:54 -0300

    feat: update score tests - back-end

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -16,7 +16,7 @@ describe("post recommendations tests", () => {
         expect(response.statusCode).toBe(422);
 
         const recommendation = await recommendationFactory.findRecommendation(recommendationData.name);
-        expect(recommendation).toBeNull;
+        expect(recommendation).toBe(null);
     });
 
     it("post a valid recommendation", async () => {
@@ -24,8 +24,8 @@ describe("post recommendations tests", () => {
         const response = await supertest(app).post("/recommendations").send(recommendationData);
         expect(response.statusCode).toBe(201);
 
-        const recommendation = await prisma.recommendation.findFirst({where: {name: recommendationData.name}});
-        expect(recommendation).not.toBeNull;
+        const recommendation = await recommendationFactory.findRecommendation(recommendationData.name);
+        expect(recommendation).not.toBe(null);
     });
 
     it("post name already registered, should fail", async () => {
@@ -35,6 +35,59 @@ describe("post recommendations tests", () => {
 
         const response2 = await supertest(app).post("/recommendations").send(recommendationData);
         expect(response2.statusCode).toBe(409);
+    });
+});
+
+describe("update score tests", () => {
+    it("send a valid id, should increment", async () => {
+        const recommendationData = recommendationFactory.validRecommendationData();
+        await supertest(app).post("/recommendations").send(recommendationData);
+        const recommendation = await recommendationFactory.findRecommendation(recommendationData.name);
+
+        const response = await supertest(app).post(`/recommendations/${recommendation.id}/upvote`);
+        expect(response.statusCode).toBe(200);
+        
+        const updatedRecommendation = await recommendationFactory.findRecommendation(recommendationData.name);
+        expect(updatedRecommendation.score).toBeGreaterThan(recommendation.score); 
+    });
+
+    it("send a valid id, should decrement", async () => {
+        const recommendationData = recommendationFactory.validRecommendationData();
+        await supertest(app).post("/recommendations").send(recommendationData);
+        const recommendation = await recommendationFactory.findRecommendation(recommendationData.name);
+
+        const response = await supertest(app).post(`/recommendations/${recommendation.id}/downvote`);
+        expect(response.statusCode).toBe(200);
+        
+        const updatedRecommendation = await recommendationFactory.findRecommendation(recommendationData.name);
+        expect(updatedRecommendation.score).toBeLessThan(recommendation.score); 
+        expect(updatedRecommendation.score).toBeGreaterThan(-4);
+    });
+
+    it("send 6 downvotes, should fail", async () => {
+        const recommendationData = recommendationFactory.validRecommendationData();
+        await supertest(app).post("/recommendations").send(recommendationData);
+        const recommendation = await recommendationFactory.findRecommendation(recommendationData.name);
+
+        await supertest(app).post(`/recommendations/${recommendation.id}/downvote`);
+        await supertest(app).post(`/recommendations/${recommendation.id}/downvote`);
+        await supertest(app).post(`/recommendations/${recommendation.id}/downvote`);
+        await supertest(app).post(`/recommendations/${recommendation.id}/downvote`);
+        await supertest(app).post(`/recommendations/${recommendation.id}/downvote`);
+        await supertest(app).post(`/recommendations/${recommendation.id}/downvote`);
+
+        const recommendation2 = await recommendationFactory.findRecommendation(recommendationData.name);
+        expect(recommendation2).toBe(null);    
+    });
+
+    it("send a invalid id, should fail", async () => {
+        const response = await supertest(app).post(`/recommendations/${10}/upvote`);
+        expect(response.statusCode).toBe(404);
+    });
+
+    it("send a invalid id, should fail", async () => {
+        const response = await supertest(app).post(`/recommendations/${10}/downvote`);
+        expect(response.statusCode).toBe(404);
     });
 });
 
