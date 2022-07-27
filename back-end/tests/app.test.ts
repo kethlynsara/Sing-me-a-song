4074421fca7c5825db0fd9080fe1a9853fb93065
Kethlyn <kethlynsaraa@gmail.com>
2022-07-27 14:32:46 -0300

    feat: change truncate query

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -5,7 +5,7 @@ import { recommendationFactory } from "./factories/recommendationFactory.js";
 
 beforeEach(async () => {
     await prisma.$transaction([
-        prisma.$executeRaw`TRUNCATE TABLE recommendations`
+        prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY`
     ])
 });
 
