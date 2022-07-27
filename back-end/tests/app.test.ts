81caab4d8af1cdf253d73f462bf2605dab37ec9e
Kethlyn <kethlynsaraa@gmail.com>
2022-07-26 21:14:11 -0300

    feat: post recommendation - test

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -5,8 +5,20 @@ import { prisma } from "../src/database.js"
 const RIGHT_LINK = "https://www.youtube.com/watch?v=rdgMPa8C-dc"
 const WRONG_LINK = "https://www.youtube"
 
+beforeEach(async () => {
+    await prisma.$executeRaw`DELETE FROM recommendations`;
+});
+
 describe("recommendations tests", () => {
-    it("", async () => {
-        
+    it("post a wrong recommendation, should fail", async () => {
+        const response = await supertest(app).post("/recommendations").send({name: "wrong music", youtubeLink: WRONG_LINK});
+        expect(response.statusCode).toBe(422);
+
+        const recommendation = await prisma.recommendation.findFirst({where: {name: "wrong music"}});
+        expect(recommendation).not.toBeNull;
     })
-})
+});
+
+afterAll(async () => {
+    await prisma.$disconnect();
+});
