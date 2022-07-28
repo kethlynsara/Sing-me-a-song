fa2d68cc4b3eb26eed3e80399dd372d8c5fec7f5
Kethlyn <kethlynsaraa@gmail.com>
2022-07-28 12:35:01 -0300

    feat: create recommendation - cypress tests

--- front-end/cypress/e2e/Home.cy.js
+++ front-end/cypress/e2e/Home.cy.js
@@ -1,11 +1,17 @@
+import { faker } from "@faker-js/faker";
+
 /// <reference types="cypress" />
 
-describe("sign up tests", () => {
-    it("sign up successfully", () => {
+describe("recommendations tests", () => {
+    it("create recommendation", () => {
       cy.visit("http://localhost:3000/");
-      cy.get("input.sc-eCYdqJ:nth-child(1)").type("Captured");
+      cy.get("input.sc-eCYdqJ:nth-child(1)").type(`${faker.random.word()}`);
       cy.get("input.sc-eCYdqJ:nth-child(2)").type("https://www.youtube.com/watch?v=Yykfw9eNA5s");
+      
+      cy.intercept("POST", "/recommendations").as("sendRecommendation");
       cy.get(".sc-jSMfEi.kVtkyd").click();
-      // <div class="sc-hKMtZM btKrnz"><input type="text" placeholder="Name" class="sc-eCYdqJ DDugM" value=""><input type="text" placeholder="https://youtu.be/..." class="sc-eCYdqJ DDugM" value=""><button class="sc-jSMfEi kVtkyd"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" color="#fff" style="color: rgb(255, 255, 255);" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M400 160l64 64-64 64"></path><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M448 224H154c-58.76 0-106 49.33-106 108v20"></path></svg></button></div>
-    })
+      cy.wait("@sendRecommendation");
+
+      cy.url().should("equal", "http://localhost:3000/");
+    });
 })
