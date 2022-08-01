fff0a52dd6adf6405e6edfb618f6a10eba403c86
Kethlyn <kethlynsaraa@gmail.com>
2022-07-31 21:20:46 -0300

    feat: home tests - cypress

--- front-end/cypress/e2e/Home.cy.js
+++ front-end/cypress/e2e/Home.cy.js
@@ -2,47 +2,101 @@ import { faker } from "@faker-js/faker";
 
 /// <reference types="cypress" />
 
-describe("recommendations tests", () => {
-    beforeEach(() => {
-      cy.resetDatabase()
-    });
+beforeEach(() => {
+  cy.resetDatabase()
+});
 
-    it("create recommendation", () => {
+describe("recommendations tests", () => {
+    it("create recommendation succesfully", () => {
       cy.visit("http://localhost:3000/");
-      cy.get("input.sc-eCYdqJ:nth-child(1)").type(`${faker.random.word()}`);
-      cy.get("input.sc-eCYdqJ:nth-child(2)").type("https://www.youtube.com/watch?v=Yykfw9eNA5s");
+      cy.get('input[placeholder="Name"]').type(`${faker.random.word()}`);
+      cy.get('input[placeholder="https://youtu.be/..."').type("https://www.youtube.com/watch?v=Yykfw9eNA5s");
+
       
       cy.intercept("POST", "/recommendations").as("sendRecommendation");
-      cy.get(".sc-jSMfEi.kVtkyd").click();
+      cy.get("button").click();
       cy.wait("@sendRecommendation");
 
       cy.url().should("equal", "http://localhost:3000/");
     });
 
-    it("votes tests", () => {
-      const recommendation = {
-        name: faker.random.word(),
-        youtubeLink: "https://www.youtube.com/watch?v=zDsE4BPuFRQ"
-      }
-  
+    it("create recommendation with invalid youtube link", () => {
       cy.visit("http://localhost:3000/");
+      cy.get('input[placeholder="Name"]').type(`${faker.random.word()}`);
+      cy.get('input[placeholder="https://youtu.be/..."').type("https://www.youtube");
+
+      
+      cy.intercept("POST", "/recommendations").as("sendRecommendation");
+      cy.get("button").click();
+      cy.wait("@sendRecommendation");
+
+      cy.url().should("equal", "http://localhost:3000/");
+      cy.get("div:last-child div").should("contain.text", "No recommendations yet! Create your own :)")
+    });
+
+    it("create recommendation with duplicated name", () => {
+      const recommendation = {
+             name: "music",
+             youtubeLink: "https://www.youtube.com/watch?v=zDsE4BPuFRQ"
+      }
+      cy.visit("http://localhost:3000/");
+
       cy.createRecommendation(recommendation);
 
-      cy.intercept("POST", "/recommendations/1/upvote").as("upvote");
-      cy.get("div.sc-iBkjds:nth-child(3)  svg:nth-child(1)",).click();
-      cy.wait("@upvote")
-    })
+      cy.request({
+        method: "POST",
+        url: 'http://localhost:5000/recommendations', 
+        failOnStatusCode: false,
+        body: recommendation
+      }).then((response) => {
+        expect(response.status).to.eq(409);
+        expect(response.body).to.eq("Recommendations names must be unique");
+      });
+
+      cy.on('window:alert',(t)=>{
+        expect(t).to.contains('Error creating recommendation!');
+     })
+    });
+
+    it("create recommendation with no data", () => {
+      cy.visit("http://localhost:3000/");
+      
+      cy.get("button").click();
+
+      cy.on("window:alert", (alert) => {
+        expect(alert).to.contains("Error creating recommendation!");
+      });
+
+      cy.url().should("equal", "http://localhost:3000/");
+      cy.get("div:last-child div").should("contain.text", "No recommendations yet! Create your own :)")
+    });
 });
 
-// describe("", () => {
-//   it("votes tests", () => {
-//     const recommendation = {
-//       name: faker.random.word(),
-//       youtubeLink: "https://www.youtube.com/watch?v=zDsE4BPuFRQ"
-//     }
+describe("upvote and downvote tests", () => {
+  it("upvote test", () => {
+    const recommendation = {
+      name: faker.random.word(),
+      youtubeLink: "https://www.youtube.com/watch?v=zDsE4BPuFRQ"
+    }
 
-//     cy.visit("http://localhost:3000/");
-//     cy.createRecommendation(recommendation);
-//     cy.get("div.sc-iBkjds:nth-child(3)  svg:nth-child(1)",).click();
-//   })
-// })
+    cy.visit("http://localhost:3000/");
+    cy.createRecommendation(recommendation);
+
+    cy.get("div:last-child article:last-child svg:first-child",).click();
+    cy.get("div:last-child article:last-child svg:first-child",).click();
+    cy.get("div:last-child article:last-child svg:first-child",).click();
+  });
+
+  it("downvote test", () => {
+    const recommendation = {
+      name: faker.random.word(),
+      youtubeLink: "https://www.youtube.com/watch?v=zDsE4BPuFRQ"
+    }
+
+    cy.visit("http://localhost:3000/");
+    cy.createRecommendation(recommendation);
+
+    cy.get("div:last-child article:last-child svg:last-child",).click();
+    cy.get("div:last-child article:last-child svg:last-child",).click();
+  });
+});
